d716b610d6e6ba70677076e811fcc3cded70c7d7
Gurman Bhatia <gurmanbh@gmail.com>
2021-12-18 14:09:04 +0530

    prototype

--- js/index.js
+++ js/index.js
@@ -1,48 +1,40 @@
 const flow = {
-    question: 'You get bitten',
+    question: 'You are a farmer in Rajasthan and get bitten by a snake while working on the fields. Where do you want to seek treatment?',
     options: [
-        {name:'Snake Stone', result: 'You die', options: [], time: 120, die: 'Y'},
-        {name:'Temple', result: 'You die', options: [], time: 120, die: 'Y'},
-        {name:'Local healer', result: 'You die', options: [], time: 180, die: 'Y'},
-        {name:'Nearest Primary Health Centre', result: 'They are out of antivenoms and you are referred to a local hospital. Do you want to go to a local hospital?',time: 60, options: [
-            {name:'Yes', result: "They give you 10 vials of antivenom. But it is not working, and your condition worsens. You are referred to a snakebite speciality hospital. Do you want to go there?", options: [
-                {name:'No', result: 'You die', options: [], die: 'Y'},
-                {name:'Yes', time: 120, result: 'The doctor at the speciality hospital wants to administer more antivenom. They give you 10 vials, but your condition is not improving. The doctor wants to give more antivenom. How many vials do you want to get?', options: [
-                    {name:'10', result: 'You die',die: 'Y',  options: [
-                        {name:'No', result: 'You die because it is ineffective. Explain antivenom.', options: [], time: 30},
-                    ]},
-                    {name:'30', result: 'You survive but you are now suffering from serum sickness.', options: [], time: 30},
-                    {name:'50', die: 'Y', result: 'You die of anaphylaxis. Explain antivenom.', options: [], time: 30 },
+        {name:'Snake Stone', result: 'You die a horrible death.', options: [], time: 120, die: 'Y'},
+        {name:'Temple', result: 'Sorry, prayers could not save you. You die a painful death.', options: [], time: 120, die: 'Y'},
+        {name:'Local healer', result: "It cost you a month's income, and you still did not survive.", options: [], time: 180, die: 'Y'},
+        {name:'Nearest Primary Health Centre', result: 'They are out of antivenoms. You are referred to a local hospital. Do you want to go to the local hospital?',time: 60, options: [
+            {name:'Yes', result: "They give you 10 vials of antivenom. But it is not working and your condition worsens. You are referred to a snakebite speciality hospital. Do you want to go there?", options: [
+                {name:'No', result: 'You are dead', options: [], die: 'Y'},
+                {name:'Yes', time: 60, result: 'The doctor at the speciality hospital wants to administer more antivenom. They give you 10 vials but your condition is not improving. The doctor suggests more antivenom. How many vials do you want to get?', options: [
+                    {name:'10', result: 'You die because the antivenom is ineffective. It was made using venom from a snake in Tamil Nadu.',die: 'Y',  options: []},
+                    {name:'30', result: 'You survive but you now suffering from serum sickness. If you get bitten again, taking the antivenom again could be lethal.', options: [], time: 30},
+                    {name:'50', die: 'Y', result: 'You die of anaphylaxis because Indian antivenom technology is outdated.', options: [], time: 30 },
                 ]},
             ], time: 120},
             {name:'No', result: 'You die', die: 'Y', options: []},
-        ]},
-        {name:'Nearest hospital', result: "They give you 10 vials of antivenom. But it is not working, and your condition worsens. You are referred to a snakebite speciality hospital. Do you want to go there?", options: [
-            {name:'No', result: 'You die', die: 'Y', options: []},
-            {name:'Yes', result: 'The doctor at the speciality hospital wants to administer more antivenom. They give you 10 vials, but your condition is not improving. The doctor wants to give more antivenom. How many vials do you want to get?', options: [
-                {name:'10', result: 'You die', die: 'Y', options: [
-                    {name:'No', result: 'You die because it is ineffective. Explain antivenom.', options: [], die: 'Y'},
-                ]},
-                {name:'30', result: 'You survive but you are now suffering from serum sickness.', options: []},
-                {name:'50', die: 'Y', result: 'You die of anaphylaxis. Explain antivenom.', options: []},
+        ]}, 
+        {name:'Nearest hospital', result: "They give you 10 vials of antivenom. But it is not working and your condition worsens. You are referred to a snakebite speciality hospital. Do you want to go there?", options: [
+            {name:'No', result: 'You are dead', options: [], die: 'Y'},
+            {name:'Yes', time: 120, result: 'The doctor at the speciality hospital wants to administer more antivenom. They give you 10 vials but your condition is not improving. The doctor suggests more antivenom. How many vials do you want to get?', options: [
+                {name:'10', result: 'You die because the antivenom is ineffective. It was made using venom from a snake in Tamil Nadu.',die: 'Y',  options: []},
+                    {name:'30', result: 'You survive but you now suffering from serum sickness. If you get bitten again, taking the antivenom again could be lethal.', options: [], time: 30},
+                    {name:'50', die: 'Y', result: 'You die of anaphylaxis because Indian antivenom technology is outdated.', options: [], time: 30 },
             ]},
-        ]},
-        {name:'Govt snakebite hospital', result: 'They are out of antivenom. Treatment at the private hospital might cost more than two lakhs. Do you want to go a private hospital instead?', options: [
-            {name:'Yes', result: 'The treatment ', result: 'The doctor at the speciality hospital wants to administer more antivenom. They give you 10 vials, but your condition is not improving. The doctor wants to give more antivenom. How many vials do you want to get?', options: [
-                {name:'10', die: 'Y', result: 'You die', options: [
-                    {name:'No', die: 'Y', result: 'You die because it is ineffective. Explain antivenom.', options: []},
-                ]},
-                {name:'30', result: 'You survive but you are now suffering from serum sickness.', options: []},
-                {name:'50', die: 'Y', result: 'You die of anaphylaxis. Explain antivenom.', options: []},
+        ], time: 120},
+        {name:'Govt snakebite hospital', time: 120,result: 'They are out of antivenom. Treatment at the private hospital might cost more than two lakhs. Do you want to go a private hospital instead?', options: [
+            {name:'Yes', time: 120, result: 'The doctor at the speciality hospital wants to administer more antivenom. They give you 10 vials, but your condition is not improving. The doctor wants to give more antivenom. How many vials do you want to get?', options: [
+                {name:'10', result: 'You die because the antivenom is ineffective. It was made using venom from a snake in Tamil Nadu.',die: 'Y',  options: []},
+                    {name:'30', result: 'You survive but you now suffering from serum sickness. If you get bitten again, taking the antivenom again could be lethal.', options: [], time: 30},
+                    {name:'50', die: 'Y', result: 'You die of anaphylaxis because Indian antivenom technology is outdated.', options: [], time: 30 },
             ]},
             {name:"No. I can't afford it.", die: 'Y', result: 'You die', options: []},
         ]},
-        {name:'Private snakebite hospital',result: 'The doctor at the speciality hospital wants to administer more antivenom. They give you 10 vials, but your condition is not improving. The doctor wants to give more antivenom. How many vials do you want to get?', options: [
-            {name:'10', die: 'Y', result: 'You die', options: [
-                {name:'No', result: 'You die because it is ineffective. Explain antivenom.', options: []},
-            ]},
-            {name:'30', result: 'You survive but you are now suffering from serum sickness.', options: []},
-            {name:'50', die: 'Y', result: 'You die of anaphylaxis. Explain antivenom.', options: []},
+        {time: 120, name:'Private snakebite hospital',result: 'The doctor at the speciality hospital wants to administer more antivenom. They give you 10 vials, but your condition is not improving. The doctor wants to give more antivenom. How many vials do you want to get?', options: [
+            {name:'10', result: 'You die because the antivenom is ineffective. It was made using venom from a snake in Tamil Nadu.',die: 'Y',  options: []},
+                    {name:'30', result: 'You survive but you now suffering from serum sickness. If you get bitten again, taking the antivenom again could be lethal.', options: [], time: 30},
+                    {name:'50', die: 'Y', result: 'You die of anaphylaxis because Indian antivenom technology is outdated.', options: [], time: 30 },
         ]},
     ]
 }
@@ -82,11 +74,11 @@ document.body.addEventListener( 'click', function ( event ) {
       timer = timer-selected.time
       console.log(timer)
         d3.select('.timer').text(timer)
-        if (selected.die === 'Y'){
+        if (selected.die === 'Y' || selected.options.length==0){
             d3.select('.timer-main').classed('hide', true)
         }
         if (timer<0){
-            d3.select('.text').text('You die.')
+            d3.select('.text').text("You didn't get the treatment on time. You died a painful death. ")
             d3.selectAll('.opt').remove()
             d3.select('.timer-main').classed('hide', true)
         }
